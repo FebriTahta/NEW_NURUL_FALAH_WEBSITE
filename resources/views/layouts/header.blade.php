f9320a64e461a8692449a035013967eafc30e013
FebriTahta <febrirtah@gmail.com>
2022-03-31 02:31:20 -0700

    first commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -194,9 +194,6 @@
                                         <li>
                                             <a href="/">Home</a>
                                         </li>
-                                        <li>
-                                            <a href="#">Contact</a>
-                                        </li>
                                         <li class="dropdown">
                                             <a href="#">Profile</a>
                                             <ul>
