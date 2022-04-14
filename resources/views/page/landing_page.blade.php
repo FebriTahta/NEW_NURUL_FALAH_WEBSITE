5ecd14af0e010d436e91c1d6a25858c4e0894f60
FebriTahta <febrirtah@gmail.com>
2022-04-14 09:47:43 +0700

    oke

--- resources/views/page/landing_page.blade.php
+++ resources/views/page/landing_page.blade.php
@@ -500,7 +500,7 @@
         </div>
     </section> --}}
 
-    <section class="providing_one jarallax" data-jarallax data-speed="0.2" data-imgPosition="50% 0%"
+    <section id="section-tabungamal" class="providing_one jarallax" data-jarallax data-speed="0.2" data-imgPosition="50% 0%"
         style="background-image: url({{ asset('asset/images/gedung.png') }})">
         <div class="container">
             <div class="row">
