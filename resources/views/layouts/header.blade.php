cd53d9ee7a44f5fa6637d696baebb129cafa5330
FebriTahta <febrirtah@gmail.com>
2022-04-25 08:59:28 +0700

    hilangkan lazis

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -157,10 +157,9 @@
             .bottom-tabungamal {
                 display: none;
             }
-            /* .mobile_foot_hide {
+            .mobile_foot_hide {
                 display: none;
-                
-            } */
+            }
             .site_footer{
                 bottom: 0;
             }
