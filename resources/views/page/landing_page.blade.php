e9a8b58db31630958da1c0d8d92ad4f4e4806fdc
FebriTahta <febrirtah@gmail.com>
2022-04-26 11:32:17 +0700

    oke

--- resources/views/page/landing_page.blade.php
+++ resources/views/page/landing_page.blade.php
@@ -126,7 +126,7 @@
                     <div class="container" style="overflow: hidden">
                         <ul id="secondary_nav">
                             @foreach ($kategori_all as $item)
-                                <li><a href="#{{ $item->name }}" style="background-color: azure; color: primary">{{ $item->name }}</a></li>
+                                <li><span href="#{{ $item->name }}" style="background-color: azure; color: primary">{{ $item->name }}</span></li>
                             @endforeach
                             <li><a href="#" style="background-color: azure">All</a></li>
                         </ul>
