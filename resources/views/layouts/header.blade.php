bd1b5267cf06577a61f876a53ab6748116ab91af
FebriTahta <febrirtah@gmail.com>
2022-04-26 13:34:23 +0700

    oke

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nurul Falah</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('tumb.jpg')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('tumb.jpg')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('tumb.jpg')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('tumb.jpeg')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('tumb.jpeg')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('tumb.jpeg')}}">
     <link rel="manifest" href="{{asset('asset/images/favicons/site.webmanifes')}}t">
     <meta name="_token" content="{{ csrf_token() }}">
     <!-- Fonts-->
