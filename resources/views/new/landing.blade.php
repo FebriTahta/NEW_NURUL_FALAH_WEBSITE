eb3816d7918699bcdef455ea19574947d4674c71
FebriTahta <febrirtah@gmail.com>
2022-05-18 16:15:12 +0700

    tampilan terbaru

--- resources/views/new/landing.blade.php
+++ resources/views/new/landing.blade.php
@@ -427,7 +427,7 @@
 		
 		{{-- <section id="why-choose" class="why-choose-section backgroud-style"> --}}
 			<section id="why-choose" class="backgroud-style">
-				<img src="{{asset('utama_hilmi.jpg')}}" alt="">
+				<img src="{{asset('hilmi_utama.jpg')}}" alt="">
 			</section>
 			
 	<!-- End why choose section
