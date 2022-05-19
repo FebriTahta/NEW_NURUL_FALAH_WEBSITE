8f31cc58bfbdc4b9ad6f7e9bc6ffdd771f2734bc
FebriTahta <febrirtah@gmail.com>
2022-05-19 13:28:52 +0700

    api

---
+++ app/Http/Controllers/ApiMediaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Helpers\ApiFormatter;
+use Illuminate\Http\Request;
+use App\Models\Posting;
+
+class ApiMediaController extends Controller
+{
+    public function berita()
+    {
+        $data = Posting::orderBy('id','desc')->whereHas('jenisposting', function($q) {
+            $q->where('name', 'berita');
+        })->paginate(5)->get();
+
+        if($data)
+        {
+            # code...
+            return ApiFormatter::createApi(200, 'success' ,$data);
+        }else {
+            # code...
+            return ApiFormatter::createApi(400, 'failed');
+        }
+    }
+}
