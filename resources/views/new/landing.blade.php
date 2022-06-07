632c04eb83dae9238180a03c04b6c1d50a0c5f02
FebriTahta <febrirtah@gmail.com>
2022-06-07 11:46:10 +0700

    program sedekah lazis

--- resources/views/new/landing.blade.php
+++ resources/views/new/landing.blade.php
@@ -170,7 +170,7 @@
 											@else
 												{{$item['judul']}}
 											@endif
-										</strong> --}}
+										</strong> --}} 
 									</div>
 									<a href="https://tabungamal.id/campaign/{{$item['slug']}}" class="item_version_2" style="padding: 5px 7px;">
 										<span class="" style="margin: 0; font-size: 12px; font-weight: 400; padding: 0;" >
