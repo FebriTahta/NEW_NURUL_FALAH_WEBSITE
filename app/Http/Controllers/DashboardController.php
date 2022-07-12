cd61c7026b4e5e0fcd560f21476370d0097451f1
FebriTahta <febrirtah@gmail.com>
2022-07-12 09:04:06 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Posting;
+use App\Models\Penulisposting;
+use App\Models\Sumberposting;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function backend_dashboard()
     {
+        $penulis = Posting::with([ 'penulisposting' => function($query) {
+            $query->where('penulis_name', 'Eko Prasetyo');
+          }])->count();
+
+        return $penulis; 
+        
         return view('backend.dashboard');
     }
 }
