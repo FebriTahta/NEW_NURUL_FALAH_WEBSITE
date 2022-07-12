8347fb665929f7398ae2e35abc3abcfd46d58d69
FebriTahta <febrirtah@gmail.com>
2022-07-12 08:57:03 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function dashboard_backend()
+    public function backend_dashboard()
     {
-        
+        return view('backend.dashboard');
     }
 }
