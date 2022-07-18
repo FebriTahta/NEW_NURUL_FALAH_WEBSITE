e91bc16e44087208ca8cbf0d3597e1d7f0813e42
FebriTahta <febrirtah@gmail.com>
2022-07-18 10:43:05 +0700

    cart

--- resources/views/backend_new/dashboard.blade.php
+++ resources/views/backend_new/dashboard.blade.php
@@ -138,81 +138,126 @@
     <script src="https://code.highcharts.com/highcharts.js"></script>
 
     <script>
+        // Highcharts.chart('chart', {
+        //     chart: {
+        //         type: 'spline'
+        //     },
+        //     title: {
+        //         text: 'Grafik Viewer Berita & Artikel'
+        //     },
+        //     subtitle: {
+        //         text: 'Sumber : Analisis https://nurulfalah.org (Febri IT)'
+        //     },
+        //     xAxis: {
+        //         categories: {!!json_encode($monthNames)!!},
+        //         accessibility: {
+        //             description: 'Months of the year'
+        //         }
+        //     },
+        //     yAxis: {
+        //         title: {
+        //             text: 'Viewer'
+        //         }
+        //         // ,
+        //         // labels: {
+        //         //     formatter: function() {
+        //         //         return this.value + '°';
+        //         //     }
+        //         // }
+        //     },
+        //     tooltip: {
+        //         crosshairs: true,
+        //         shared: true
+        //     },
+        //     plotOptions: {
+        //         spline: {
+        //             marker: {
+        //                 radius: 4,
+        //                 lineColor: '#666666',
+        //                 lineWidth: 1
+        //             }
+        //         }
+        //     },
+        //     series: [
+        //         {
+        //         name: 'Berita',
+        //         marker: {
+        //             symbol: 'square'
+        //         },
+        //         data : {!!json_encode($berita)!!}
+        //         // data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+        //         //     y: 26.5,
+        //         //     marker: {
+        //         //         symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+        //         //     },
+        //         //     accessibility: {
+        //         //         description: 'Sunny symbol, this is the warmest point in the chart.'
+        //         //     }
+        //         // }, 23.3, 18.3, 13.9, 9.6]
+
+        //         }, {
+        //             name: 'Artikel',
+        //             marker: {
+        //                 symbol: 'diamond'
+        //             },
+        //             data : {!!json_encode($artikel)!!},
+        //             // data: [{
+        //             //     y: 3.9,
+        //             //     marker: {
+        //             //         symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
+        //             //     },
+        //             //     accessibility: {
+        //             //         description: 'Snowy symbol, this is the coldest point in the chart.'
+        //             //     }
+        //             // }, {!!json_encode($artikel)!!}]
+        //         }
+        //     ]
+        // });
+
         Highcharts.chart('chart', {
-            chart: {
-                type: 'spline'
-            },
+        title: {
+            text: 'New User Growth, 2020'
+        },
+        subtitle: {
+            text: 'Source: positronx.io'
+        },
+        xAxis: {
+            categories: {!!json_encode($monthNames)!!}
+        },
+        yAxis: {
             title: {
-                text: 'Grafik Viewer Berita & Artikel'
-            },
-            subtitle: {
-                text: 'Sumber : Analisis https://nurulfalah.org (Febri IT)'
-            },
-            xAxis: {
-                categories: {!!json_encode($monthNames)!!},
-                accessibility: {
-                    description: 'Months of the year'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Viewer'
-                }
-                // ,
-                // labels: {
-                //     formatter: function() {
-                //         return this.value + '°';
-                //     }
-                // }
-            },
-            tooltip: {
-                crosshairs: true,
-                shared: true
-            },
-            plotOptions: {
-                spline: {
-                    marker: {
-                        radius: 4,
-                        lineColor: '#666666',
-                        lineWidth: 1
+                text: 'Number of New Users'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true
+            }
+        },
+        series: [{
+            name: 'New Users',
+            data: {!!json_encode($artikel)!!}
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
                     }
                 }
-            },
-            series: [
-                {
-                name: 'Berita',
-                marker: {
-                    symbol: 'square'
-                },
-                data : {!!json_encode($berita)!!}
-                // data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-                //     y: 26.5,
-                //     marker: {
-                //         symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
-                //     },
-                //     accessibility: {
-                //         description: 'Sunny symbol, this is the warmest point in the chart.'
-                //     }
-                // }, 23.3, 18.3, 13.9, 9.6]
-
-                }, {
-                    name: 'Artikel',
-                    marker: {
-                        symbol: 'diamond'
-                    },
-                    data : {!!json_encode($artikel)!!},
-                    // data: [{
-                    //     y: 3.9,
-                    //     marker: {
-                    //         symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
-                    //     },
-                    //     accessibility: {
-                    //         description: 'Snowy symbol, this is the coldest point in the chart.'
-                    //     }
-                    // }, {!!json_encode($artikel)!!}]
-                }
-            ]
-        });
+            }]
+        }
+    });
 
         $(document).ready(function (){
             console.log({!!json_encode($artikel)!!})
