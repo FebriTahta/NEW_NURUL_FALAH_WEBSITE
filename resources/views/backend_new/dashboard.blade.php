654aa986b50e8a7416268abe2c3db70cb5ec4858
FebriTahta <febrirtah@gmail.com>
2022-07-18 10:19:09 +0700

    cart

--- resources/views/backend_new/dashboard.blade.php
+++ resources/views/backend_new/dashboard.blade.php
@@ -590,15 +590,15 @@
                     marker: {
                         symbol: 'diamond'
                     },
-                    data: [{
-                        y: 3.9,
+                    data: {
+                        // y: 3.9,
                         marker: {
                             symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
                         },
                         accessibility: {
                             description: 'Snowy symbol, this is the coldest point in the chart.'
                         }
-                    }, {!!json_encode($artikel)!!}]
+                    }, {!!json_encode($artikel)!!}
                 }
             ]
         });
