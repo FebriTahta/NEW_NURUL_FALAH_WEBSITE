4e17c6c7576711fef4e7b3d851214ab6a1d4ce79
FebriTahta <febrirtah@gmail.com>
2022-07-18 15:33:47 +0700

    best berita

--- resources/views/new_layouts/be_sidebar.blade.php
+++ resources/views/new_layouts/be_sidebar.blade.php
@@ -121,28 +121,8 @@
             <li class="header"><strong>MAIN NAVIGATION</strong></li>
             <li class="treeview"><a href="#">
                 <i class="icon icon-sailing-boat-water purple-text s-18"></i> <span>Dashboard</span> <i
-                    class="icon icon-angle-left s-18 pull-right"></i>
+                    class="icon icon-angle-left s-18"></i>
             </a>
-                <ul class="treeview-menu">
-                    <li><a href="index.html"><i class="icon icon-folder5"></i>Panel Dashboard 1</a>
-                    </li>
-                    <li><a href="panel-page-dashboard1-rtl.html"><i class="icon icon-folder5"></i>Panel Dashboard 1 - RTL</a>
-                    </li>
-                    <li><a href="panel-page-dashboard2.html"><i class="icon icon-folder5"></i>Panel Dashboard 2</a>
-                    </li>
-                    <li><a href="panel-page-dashboard3.html"><i class="icon icon-folder5"></i>Panel Dashboard 3</a>
-                    </li>
-                    <li><a href="panel-page-dashboard4.html"><i class="icon icon-folder5"></i>Panel Dashboard 4</a>
-                    </li>
-                    <li><a href="panel-page-dashboard5.html"><i class="icon icon-folder5"></i>Panel Dashboard 5</a>
-                    </li>
-                    <li><a href="panel-page-dashboard6.html"><i class="icon icon-folder5"></i>Panel Dashboard 6</a>
-                    </li>
-                    <li><a href="panel-page-dashboard7.html"><i class="icon icon-folder5"></i>Panel Dashboard 7</a></li>
-                    <li><a href="panel-page-dashboard9.html"><i class="icon icon-folder5"></i>Panel Dashboard 9</a></li>
-                    <li><a href="panel-page-dashboard10.html"><i class="icon icon-folder5"></i>Panel Dashboard 10</a></li>
-                    <li><a href="panel-page-dashboard11.html"><i class="icon icon-folder5"></i>Panel Dashboard 11</a></li>
-                </ul>
             </li>
             <li class="treeview"><a href="#">
                 <i class="icon icon icon-package blue-text s-18"></i>
