fa1e73096841541b3b9d0420aa87d8005167d611
FebriTahta <febrirtah@gmail.com>
2022-07-18 14:52:03 +0700

    best berita

--- resources/views/new/list_berita_artikel.blade.php
+++ resources/views/new/list_berita_artikel.blade.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Daftar {{$jenis}}</title>
+<title style="text-transform: capitalize">Daftar {{$jenis}}</title>
 <link rel="apple-touch-icon" sizes="180x180" href="{{asset('tumb.jpeg')}}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{asset('tumb.jpeg')}}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{asset('tumb.jpeg')}}">
