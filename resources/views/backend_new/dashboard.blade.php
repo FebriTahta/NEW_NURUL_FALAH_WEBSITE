118107e77b076ee85ed105cd6e72f33ce697905a
FebriTahta <febrirtah@gmail.com>
2022-07-18 10:26:25 +0700

    cart

--- resources/views/backend_new/dashboard.blade.php
+++ resources/views/backend_new/dashboard.blade.php
@@ -551,9 +551,9 @@
                     text: 'Viewer'
                 },
                 labels: {
-                    formatter: function() {
-                        return this.value + '°';
-                    }
+                    // formatter: function() {
+                    //     return this.value + '°';
+                    // }
                 }
             },
             tooltip: {
@@ -598,7 +598,7 @@
                         accessibility: {
                             description: 'Snowy symbol, this is the coldest point in the chart.'
                         }
-                    }, {!! json_decode('',$artikel) !!}]
+                    }, {!!json_encode($artikel)!!}]
                 }
             ]
         });
