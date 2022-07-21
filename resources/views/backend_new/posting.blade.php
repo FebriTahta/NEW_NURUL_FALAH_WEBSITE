60d259727bddc234e19686099b42a39498f6e598
FebriTahta <febrirtah@gmail.com>
2022-07-21 15:48:24 +0700

    psot

--- resources/views/backend_new/posting.blade.php
+++ resources/views/backend_new/posting.blade.php
@@ -89,8 +89,7 @@
                                     <div class="col-md-12 col-12">
                                         <div class="form-group">
                                             <label for="deskripsi"><b>Deskripsi</b></label>
-                                            <textarea name="deskripsi" id="deskripsi" class="my-editor form-control" cols="30" rows="30"
-                                            required></textarea>
+                                            <textarea name="deskripsi" id="deskripsi" class="my-editor form-control" cols="30" rows="30"></textarea>
                                         </div>
                                     </div>
                                     <div class="col-md-6 col-12">
@@ -140,38 +139,38 @@
 //    });
 
    
-//    tinymce.init({
-//             selector: '#deskripsi',
-//             plugins: 'image code',
-//             toolbar: 'undo redo | bold italic underline | link image | code',
-//             image_title: true,
-//             automatic_uploads: true,
-//             file_picker_types: 'image',
-//             file_picker_callback: function(cb, value, meta) {
-//                 var input = document.createElement('input');
-//                 input.setAttribute('type', 'file');
-//                 input.setAttribute('accept', 'image/*');
-//                 input.onchange = function() {
-//                     var file = this.files[0];
+   tinymce.init({
+            selector: '#deskripsi',
+            plugins: 'image code',
+            toolbar: 'undo redo | bold italic underline | link image | code',
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+            file_picker_callback: function(cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+                input.onchange = function() {
+                    var file = this.files[0];
 
-//                     var reader = new FileReader();
-//                     reader.onload = function() {
-//                         var id = 'blobid' + (new Date()).getTime();
-//                         var blobCache = tinymce.activeEditor.editorUpload.blobCache;
-//                         var base64 = reader.result.split(',')[1];
-//                         var blobInfo = blobCache.create(id, file, base64);
-//                         blobCache.add(blobInfo);
+                    var reader = new FileReader();
+                    reader.onload = function() {
+                        var id = 'blobid' + (new Date()).getTime();
+                        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                        var base64 = reader.result.split(',')[1];
+                        var blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
 
-//                         cb(blobInfo.blobUri(), {
-//                             title: file.name
-//                         });
-//                     };
-//                     reader.readAsDataURL(file);
-//                 };
+                        cb(blobInfo.blobUri(), {
+                            title: file.name
+                        });
+                    };
+                    reader.readAsDataURL(file);
+                };
 
-//                 input.click();
-//             }
-//         });
+                input.click();
+            }
+        });
 
 
         function showPreview(event) {
