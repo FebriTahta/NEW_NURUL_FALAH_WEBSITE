462529e75e2cd8d82d49d1154f2101060f0f753f
FebriTahta <febrirtah@gmail.com>
2022-07-25 10:37:47 +0700

    pelatihan

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -83,7 +83,7 @@ class PostController extends Controller
     public function backend_list_posting(Request $request)
     {
         if ($request->ajax()) {
-            $data = Posting::with('jenisposting','kategoriposting','penulisposting','sumberposting')->orderBy('id','desc');
+            $data = Posting::with('jenisposting','kategoriposting','penulisposting','sumberposting')->orderBy('id','desc')->get();
             return Datatables::of($data)
                 ->addIndexColumn()
                 ->addColumn('jenis', function($data){
